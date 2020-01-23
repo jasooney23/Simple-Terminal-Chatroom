62753e8852420305ca37f3866ae90f30bfa28ffd
jasooney23 <56005473+jasooney23@users.noreply.github.com>
2020-01-23 11:29:45 -0800

    Update Client_Script.py

--- Client_Script.py
+++ Client_Script.py
@@ -1,51 +1,24 @@
-import socket, json, threading
+import socket, threading
 
-host = input("\nIPV4 address to bind to:\n")
+ip = input("\nIPV4 adress to bind to:\n")
 port = int(input("\nPort to bind to:\n"))
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-jsonList = {"command" : "message", "args" : "Hello world!"}
 
 #====================================#
-
-def formatToMessage(obj):
-    string = json.dumps(obj)
-    string += "\n"
-    return string.encode("utf-8")
 
 def receiveMessages(sock):
     # Thread mainloop
     while True:
         message = sock.recv(4096)
-        message = json.loads(message)
-
-        command = message["command"]
-        # Decision maker
-        if command == "message":
-            print(">>Them: " + message["args"])
-        elif command == "disconnect":
-            sock.close()
-            print("Disconnected\n")
-            break
+        print(message.decode())
 
 #====================================#
 
-sock.connect((host, port))
+sock.connect((ip, port))
 
 receiver = threading.Thread(target=receiveMessages, args=(sock,))
 receiver.start()
 
-try:
-    # Main thread: send message
-    while True:
-        jsonList["args"] = input()
-        jsonList["command"] = "message"
-        print(">>You: " + jsonList["args"])
-        sock.send(formatToMessage(jsonList))
-except KeyboardInterrupt as interrupt:
-    # Shutdown:
-    jsonList["command"] = "close"
-    print("\nDisconnecting... ", end="")
-    sock.send(formatToMessage(jsonList))
-
-receiver.join()
-input("\nPress <enter> to exit\n")
+# Main thread: send message
+while True:
+    sock.send(input().encode('UTF-8'))
